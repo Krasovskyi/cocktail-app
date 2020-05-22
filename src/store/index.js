a85788423b92db37c43b56026a3b696aa7315de9
Krasovskyi <30krasovsky@gmail.com>
2020-05-23 02:13:52 +0300

    delete 1 unnecessary import

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Vuex from 'vuex';
 import filters from './modules/filters'
 import cocktails from './modules/cocktails'
-import axios from 'axios'
+
 
 Vue.use(Vuex);
 
